64ea4600358ca70a09aafd27a26a91a660396cc6
Lovelex <lukas489@gmail.com>
2025-01-28 22:54:51 -0300

    primeiros passos, backend

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+console.log('app.module.ts');
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+
+        // 4) Se 'synchronize' é true, o TypeORM cria/atualiza tabelas automaticamente
+        // (bom para ambiente de desenvolvimento, mas CUIDADO em produção!)
+        synchronize: configService.get<boolean>('DB_SYNC'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
